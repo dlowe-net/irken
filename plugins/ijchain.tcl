3a05d87853af2635d154f2fe3f1ee74ea1af6ffe
Daniel Lowe <dlowe@dlowe.net>
2019-10-05 11:38:38 -0400

    Add support for ischain bot in #tcl channel

--- plugins/ijchain.tcl
+++ plugins/ijchain.tcl
@@ -2,58 +2,69 @@
 #
 # Description:
 #
-#   ijchains is a bot bridging a jabber chat to the #tcl channel on
-#   freenode.net.  This plugin makes messages sent through the bot
-#   appear as if they are occurring in the IRC channel.
+#   ijchain and ischain are bots bridging jabber and slack chat to the
+#   #tcl channel on freenode.net.  This plugin makes messages sent
+#   through the bots appear as if they are occurring in the IRC
+#   channel.
 
 namespace eval ijchain {
     namespace import ::irc::* ::irken::*
-    variable botnick "ijchain"
+    variable bots {ijchain ischain}
 
-    proc decoratenick {nick} {
-        return [string cat $nick "@"]
+    proc decoratenick {bot nick} {
+        if {$bot eq "ijchain"} {
+            return [string cat $nick "@"]
+        }
+        return [string cat $nick "%"]
     }
 
-    hook handleJOIN ijchain 75 {serverid msg} {
-        variable botnick
-        if {![isself $serverid [dict get $msg src]]} {
+    proc isbotnick {nick} {
+        variable bots
+        return [expr {[lsearch -exact $bots $nick] == -1}]
+    }
+
+    hook handleJOIN ijchain 75 {chanid msg} {
+        variable bots
+        if {![isself [irken::serverpart $chanid] [dict get $msg src]]} {
             return
         }
         if {[lindex [dict get $msg args] 0] ne "#tcl"} {
             return
         }
-        send $serverid "PRIVMSG $botnick :names"
+        foreach bot $bots {
+            send [irken::serverpart $chanid] "PRIVMSG $bot :names"
+        }
     }
 
     hook ctcpACTION ijchain 30 {chanid msg text} {
-        variable botnick
-        if {[dict get $msg src] ne $botnick} {
+        set bot [dict get $msg src]
+        if {[isbotnick $bot]} {
             return
         }
-        if {[regexp -- {^(\S+) has become available} $text -> nick]} {
-            hook call handleJOIN [irken::serverpart $chanid] [dict replace $msg src [decoratenick $nick]]
+        if {[regexp -- {^(\S+) has become available$} $text -> nick]} {
+            hook call handleJOIN [irken::serverpart $chanid] [dict replace $msg src [decoratenick $bot $nick]]
             return -code break
         }
-        if {[regexp -- {^(\S+) has left} $text -> nick]} {
-            hook call handlePART [irken::serverpart $chanid] [dict replace $msg src [decoratenick $nick] args [lrange [dict get $msg args] 0 0] trailing {}]
+        if {[regexp -- {^(\S+) has left$} $text -> nick]} {
+            hook call handlePART [irken::serverpart $chanid] [dict replace $msg src [decoratenick $bot $nick] args [lrange [dict get $msg args] 0 0] trailing {}]
             return -code break
         }
         if {[regexp -- {^(\S+) (.*)} $text  -> nick text]} {
-            return -code continue [list $chanid [dict replace $msg src [decoratenick $nick]] $text]
+            return -code continue [list $chanid [dict replace $msg src [decoratenick $bot $nick]] $text]
         }
     }
 
     hook handlePRIVMSG ijchain 15 {serverid msg} {
-        variable botnick
-        if {[dict get $msg src] ne $botnick} {
+        set bot [dict get $msg src]
+        if {[isbotnick $bot]} {
             return
         }
         if {[ischannel [chanid $serverid [lindex [dict get $msg args] 0]]]} {
             # On channel
             if {[regexp -- {^<([^>]+)> (.*)} [dict get $msg trailing] -> nick text]} {
-                return -code continue [list $serverid [dict replace $msg src [decoratenick $nick] trailing $text]]
+                return -code continue [list $serverid [dict replace $msg src [decoratenick $bot $nick] trailing $text]]
             } elseif {[regexp -- {^(\w+) (.*)} [dict get $msg trailing] -> nick text]} {
-                return -code continue [list $serverid [dict replace $msg src [decoratenick $nick] trailing "\001ACTION $text\001"]]
+                return -code continue [list $serverid [dict replace $msg src [decoratenick $bot $nick] trailing "\001ACTION $text\001"]]
             }
             return
         }
@@ -65,7 +76,7 @@ namespace eval ijchain {
 
         # Must be the names of correspondents
         foreach nick [split [dict get $msg trailing] " "] {
-            lappend names [decoratenick $nick]
+            lappend names [decoratenick $bot $nick]
         }
         hook call handle353 $serverid \
             [dict create args [list "*" "#tcl"] trailing $names]
