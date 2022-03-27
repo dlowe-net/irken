586a03b10cf839b4644d025a87086fc61baed14d
Daniel Lowe <dlowe@dlowe.net>
2022-03-27 08:15:36 -0400

    Allow debug window to be toggled

--- plugins/debug.tcl
+++ plugins/debug.tcl
@@ -1,5 +1,7 @@
 namespace eval ::irken::debug {
 
+    variable enabled true
+
     hook cmdCOLORTEST debug 50 {serverid arg} {
         set text {}
         dict for {code color} $::codetagcolormap {
@@ -14,50 +16,71 @@ namespace eval ::irken::debug {
         addchantext $::active "*" $text {}
     }
 
-    set enabled true
+    proc setupwin {} {
+        toplevel .debug
+        wm title .debug "Irken Debug"
+        text .debug.t -state disabled
+        pack .debug.t -fill both -expand 1
+        .debug.t tag config input -foreground blue
+        .debug.t tag config output -foreground red
+        bind .debug <Destroy> {if {"%W" == ".debug"} {set ::irken::debug::enabled false}}
+    }
 
-    if {$enabled} {
-        hook setupui debug 50 {} {
-            toplevel .debug
-            wm title .debug "Irken Debug"
-            text .debug.t -state disabled
-            pack .debug.t -fill both -expand 1
-            .debug.t tag config input -foreground blue
-            .debug.t tag config output -foreground red
+    hook cmdDEBUG debug 50 {serverid arg} {
+        variable enabled
+
+        if {$enabled} {
+            set enabled false
+            destroy .debug
+        } else {
+            set enabled true
+            setupwin
         }
+    }
 
-        proc insert {tag text} {
-            set atbottom [expr {[lindex [.debug.t yview] 1] == 1.0}]
-            .debug.t configure -state normal
-            .debug.t insert end $text [list $tag]
-            if {$atbottom} {
-                .debug.t yview end
-            }
-            .debug.t configure -state disabled
+    hook setupui debug 50 {} {
+        variable enabled
+        set enabled true
+        setupwin
+    }
+
+    proc insert {tag text} {
+        set atbottom [expr {[lindex [.debug.t yview] 1] == 1.0}]
+        .debug.t configure -state normal
+        .debug.t insert end $text [list $tag]
+        if {$atbottom} {
+            .debug.t yview end
         }
+        .debug.t configure -state disabled
+    }
 
-        namespace eval ::irc {
-            namespace export send
-            proc send {serverid str} {
+    namespace eval ::irc {
+        namespace export send
+        proc send {serverid str} {
+            variable ::irken::debug::enabled
+            if {$enabled} {
                 ::irken::debug::insert output "$serverid <- $str\u21b5\n"
-                set chan [dict get $::serverinfo $serverid chan]
-                try {
-                    puts $chan $str
-                } on error {err} {
-                    irken::addchantext $serverid "WRITE ERROR: $err" -tags system
-                }
-                flush $chan
             }
+            set chan [dict get $::serverinfo $serverid chan]
+            try {
+                puts $chan $str
+            } on error {err} {
+                irken::addchantext $serverid "WRITE ERROR: $err" -tags system
+            }
+            flush $chan
         }
+    }
 
-        namespace eval ::irken {
-            proc recv {chan} {
-                if {[catch {gets $chan line} len] || [eof $chan]} {
-                    disconnected $chan
-                } elseif {$len != 0 && [set msg [parseline [string trimright [encoding convertfrom utf-8 $line]]]] ne ""} {
+    namespace eval ::irken {
+        proc recv {chan} {
+            variable ::irken::debug::enabled
+            if {[catch {gets $chan line} len] || [eof $chan]} {
+                disconnected $chan
+            } elseif {$len != 0 && [set msg [parseline [string trimright [encoding convertfrom utf-8 $line]]]] ne ""} {
+                if {$enabled} {
                     ::irken::debug::insert input "[dict get $::servers $chan] -> $line\u21b5\n"
-                    hook call [expr {[hook exists "handle[dict get $msg cmd]"] ? "handle[dict get $msg cmd]":"handleUnknown"}] [dict get $::servers $chan] $msg
                 }
+                hook call [expr {[hook exists "handle[dict get $msg cmd]"] ? "handle[dict get $msg cmd]":"handleUnknown"}] [dict get $::servers $chan] $msg
             }
         }
     }
