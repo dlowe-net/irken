f0480b465868735e73317b6e1f274b5b3d54c8b4
Aaron Bieber <aaron@bolddaemon.com>
2025-05-27 07:24:59 -0600

    Add some fixes
    - remove debug statement
    - add error checking
    - remove setting of window title
    - fix "servirid" typo

--- plugins/upload.tcl
+++ plugins/upload.tcl
@@ -2,6 +2,12 @@
 #
 # Description:
 #
+#    Open up the file picker and upload the file to envs.sh.
+#
+# Requirements:
+#
+#    This plugin requires installation of tclcurl.
+#
 # Adds commands:
 #    /upload - Prompt for a file to upload to envs.sh
 
@@ -11,26 +17,27 @@ namespace eval ::irken::upload {
     namespace import ::irc::* ::irken::*
 
     proc postFile { url sendFile } {
-	set fp [open $sendFile r]
-	fconfigure $fp -translation binary
-	set fileContents [read $fp]
-	close $fp
+	if {![catch {open $sendFile r} fp]} {
+	    fconfigure $fp -translation binary
+	    set fileContents [read $fp]
+	    close $fp
 
-	set httpBody ""
-	set resp [curl::transfer -url $url \
-		      -post 1 -bodyvar httpBody \
-		      -httppost [list name "file" bufferName [file tail $sendFile] buffer $fileContents]]
-	puts $resp
-	return [string trim $httpBody]
+	    set httpBody ""
+	    if {![catch {curl::transfer -url $url \
+			     -post 1 -bodyvar httpBody \
+			     -httppost [list name "file" bufferName [file tail $sendFile] buffer $fileContents]}]} {
+
+		return [string trim $httpBody]
+	    } else {
+		addchantext $::active "Warning: unable to upload: $httpBody" -tags {fg_red italic}
+	    }
+	} else {
+	    addchantext $::active "Warning: unable to open $sendFile" -tags {fg_red italic}
+	}
     }
 
-    hook cmdUPLOAD upload 50 {servirid arg} {
-	wm title . "Irken File Uploader"
-
-	set types {
-	    {"All Files" *}
-	}
-	set filename [tk_getOpenFile -filetypes $types -title "Choose a file"]
+    hook cmdUPLOAD upload 50 {serverid arg} {
+	set filename [tk_getOpenFile -filetypes {{{Images} {.png .jpg .jpeg .gif .PNG .JPG .JPEG .GIF}} {{All Files} {*}}} -title "Choose a file"]
 	if {$filename ne ""} {
 	    set url https://envs.sh
 	    hook call cmdMSG [serverpart $::active] "[channelpart $::active] [postFile $url $filename]"
